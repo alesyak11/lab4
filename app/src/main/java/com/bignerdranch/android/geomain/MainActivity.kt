ae6efa3c9123a73e1bd3e436b8b9a6e491b34815
Dmitriy <dmitriybogatyrev2001@gmail.com>
2023-12-12 10:51:29 +0700

    4.1 закончено

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -2,11 +2,15 @@ package com.bignerdranch.android.geomain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -23,9 +27,14 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_asia, true))
     private var currentIndex = 0
 
+    private var answered = false
+    private var correctAnswersCount = 0
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -35,10 +44,16 @@ class MainActivity : AppCompatActivity() {
 
 
         trueButton.setOnClickListener { view: View ->
-            checkAnswer(true)
+            if (!answered) {
+                checkAnswer(true)
+                disableAnswerButtons()
+            }
         }
         falseButton.setOnClickListener { view: View ->
-            checkAnswer(false)
+            if (!answered) {
+                checkAnswer(false)
+                disableAnswerButtons()
+            }
         }
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
@@ -51,19 +66,73 @@ class MainActivity : AppCompatActivity() {
 
         updateQuestion()
     }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
-        questionTextView.setText(questionTextResId)
+        enableAnswerButtons()
+        if (currentIndex < questionBank.size) {
+            val questionTextResId = questionBank[currentIndex].textResId
+            questionTextView.setText(questionTextResId)
+        } else {
+            showFinalScore()
+        }
     }
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
         val messageResId = if (userAnswer == correctAnswer) {
+            correctAnswersCount++
             R.string.correct_toast
         }
         else {
             R.string.incorrect_toast
         }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
-            .show()
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        if (currentIndex == questionBank.size - 1) {
+            showFinalScore()
+        }
+    }
+    private fun disableAnswerButtons() {
+        trueButton.isEnabled = false
+        falseButton.isEnabled = false
+        answered = true
+    }
+    private fun enableAnswerButtons() {
+        trueButton.isEnabled = true
+        falseButton.isEnabled = true
+        answered = false
+    }
+    private fun showFinalScore() {
+        val totalQuestions = questionBank.size
+        val percentage = (correctAnswersCount.toDouble() / totalQuestions.toDouble()) * 100
+
+        val message = "Вы ответили правильно на $correctAnswersCount из $totalQuestions вопросов. Ваша оценка: %.2f%%".format(percentage)
+
+        // Используем AlertDialog для отображения уведомления
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("Результат")
+        alertDialogBuilder.setMessage(message)
+        alertDialogBuilder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 }
