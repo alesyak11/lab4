f4ddf0c84309ed4cd98c9df90adf23bd230b9beb
Dmitriy <dmitriybogatyrev2001@gmail.com>
2023-12-15 08:16:22 +0700

    4.2 добавила вторую активность и кнопку cheat

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.geomain
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,17 +9,21 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.snackbar.Snackbar
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
 
+private const val REQUEST_CODE_CHEAT = 0
+
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: ImageButton
     private lateinit var prevButton: ImageButton
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView: TextView
 
     private var correctAnswersCount = 0
@@ -38,6 +44,7 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         prevButton = findViewById(R.id.prev_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
 
 
@@ -59,6 +66,13 @@ class MainActivity : AppCompatActivity() {
                 nextButton.visibility = View.INVISIBLE
             }
         }
+        cheatButton.setOnClickListener {
+        // Начало CheatActivity
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+
+        }
         prevButton.setOnClickListener {
             quizViewModel.moveToBack()
             updateQuestion()
@@ -66,6 +80,19 @@ class MainActivity : AppCompatActivity() {
 
         updateQuestion()
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
+    {
+        super.onActivityResult(requestCode,
+            resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            quizViewModel.isCheater =
+                data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+    }
+
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart() called")
@@ -108,14 +135,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = quizViewModel.currentQuestionAnswer
-        val messageResId = if (userAnswer == correctAnswer) {
-            correctAnswersCount++
-            getString(R.string.correct_toast)
+        val correctAnswer: Boolean = quizViewModel.currentQuestionAnswer
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
-        else {
-            getString(R.string.incorrect_toast)
-        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
         Snackbar.make(findViewById(android.R.id.content), messageResId, Snackbar.LENGTH_SHORT).show()
 
         if (quizViewModel.equalCurQB) {
